0651e50622869ef7b9bf4cae4e355933194b7394
Drew Hund <languageinvestigator@gmail.com>
2021-01-11 19:37:53 -0500

    built photos controller

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,56 @@
+class PhotosController < ApplicationController
+  before_action :find_photo, except: [:new, :create]
+
+  def new
+    @photo = current_user.photos.build
+  end
+
+  def create
+    @photo = current_user.photos.build(photo_params)
+    @photo.image.attach(params[:photo][:image])
+
+    if @photo.save
+      flash[:notice] = "Add tags to make this entry searchable!"
+      redirect_to edit_photo_path
+    else
+      flash[:notice] = "Something went wrong, please try again"
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @photo.update(permitted_params)
+    tags.each { |tag| @photo.tags.create(name: tag) }
+  end
+
+  def destroy
+    @photo.destroy
+    flash[:notice] = "Photo successfulyl removed"
+    redirect_to my_photos_path
+  end
+
+  private
+
+    def find_photo
+      @photo = Photo.find(params[:id])
+    end
+
+    def photo_params
+      permitted_params.except(:tags)
+    end
+
+    def permitted_params
+      params.require(:photo).permit(:title, :summary, :image, :tags)
+    end
+
+    def tags
+      permitted_params.slice(:tags).reject(&:empty?)
+    end
+
+end
--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,3 +1,5 @@
 class Meeting < ApplicationRecord
   belongs_to :user
+
+  validates :title, :description, presence: true
 end
