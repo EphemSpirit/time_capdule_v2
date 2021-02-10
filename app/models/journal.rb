d6b53ca15ae4b7fa7adfa225bff4cb4de8e6c2ef
Drew Hund <languageinvestigator@gmail.com>
2021-02-10 15:57:43 -0500

    removed mdoe aliasing on Journal

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -1,5 +1,6 @@
 class Journal < ApplicationRecord
-  belongs_to :author, class_name: "User", foreign_key: :author_id
+  #belongs_to :author, class_name: "User", foreign_key: :author_id
+  belongs_to :user
   has_many :tags, as: :taggable
 
   validates :title, :body, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_17_153942) do
+ActiveRecord::Schema.define(version: 2021_02_10_200101) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -60,11 +60,11 @@ ActiveRecord::Schema.define(version: 2021_01_17_153942) do
 
   create_table "journals", force: :cascade do |t|
     t.string "title"
-    t.integer "author_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "body"
-    t.index ["author_id"], name: "index_journals_on_author_id"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_journals_on_user_id"
   end
 
   create_table "meetings", force: :cascade do |t|
